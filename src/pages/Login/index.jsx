ab57ce3e742380d490d86e44208748ba3cb2367b
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-26 04:33:15 +0200

    login works

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,9 +3,22 @@ import "./style.css";
 import google from "../../assets/google.svg";
 import linkedin from "../../assets/linkedin.svg";
 import bg from "../../assets/bg.svg";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
 const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!credentials.email.includes("@")) {
+      setError("Invalid email");
+    } else if (credentials.password.length < 6) {
+      setError("Short password");
+    } else {
+      setError("");
+    }
+  }, [credentials]);
   return (
     <div>
       <nav className="navbar">
@@ -33,15 +46,37 @@ const Login = () => {
                 <h1>Find a Job through your community</h1>
                 <form action="" method="post">
                   <div className="line">
-                    <label htmlFor="email">Email or phone</label>
+                    <label htmlFor="email">Email </label>
                     <div className="field">
-                      <input type="text" className="input" placeholder="" />
+                      <input
+                        type="text"
+                        className="input"
+                        placeholder=""
+                        onChange={(e) => {
+                          console.log(e.target.value);
+                          setCredentials({
+                            ...credentials,
+                            email: e.target.value,
+                          });
+                        }}
+                      />
                     </div>
                   </div>
                   <div className="line">
                     <label htmlFor="email">Password</label>
                     <div className="field">
-                      <input type="text" className="input" placeholder="" />
+                      <input
+                        type="text"
+                        className="input"
+                        placeholder=""
+                        onChange={(e) => {
+                          console.log(e.target.value);
+                          setCredentials({
+                            ...credentials,
+                            password: e.target.value,
+                          });
+                        }}
+                      />
                       <span className="icon">display</span>
                     </div>
                   </div>
@@ -50,7 +85,40 @@ const Login = () => {
                   </a>
 
                   <div className="btns">
-                    <button className="btn-login">login</button>
+                    <button
+                      className="btn-login"
+                      onClick={async (e) => {
+                        e.preventDefault();
+                        try {
+                          const formData = new FormData();
+                          const { email, password } = credentials;
+                          formData.append("email", email);
+                          formData.append("password", password);
+                          
+                          const response = await axios.post(
+                            "http://localhost/linkedin_clone_backend/signin.php",
+                            formData
+                          );
+
+                          console.log(response.data);
+
+                          if (response.data.status === "success") {
+                            console.log("login success");
+                          } else {
+                            setError(
+                              "Login failed. Please check your credentials."
+                            );
+                          }
+                        } catch (error) {
+                          console.error(error);
+                          setError(
+                            "An error occurred during login. Please try again later."
+                          );
+                        }
+                      }}
+                    >
+                      login
+                    </button>
                   </div>
                 </form>
 
